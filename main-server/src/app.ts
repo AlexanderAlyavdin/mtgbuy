4b8bdd5bdb98e5623bebd2fd24b463e8927ce46f
sevoster <sevoster@gmail.com>
2020-02-29 16:12:47 +0300

    Parse {name price link quantity} from mtgsale search

--- main-server/src/app.ts
+++ main-server/src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-const request = require('request');
+import { JSDOM } from 'jsdom';
 
 const app = express();
 const port = process.env.PORT || 3030;
@@ -14,9 +14,34 @@ app.use((req, res, next) => {
 
 app.get('/', (req, response) => {
   const cardName = req.query.cardName;
-  request(`https://mtgsale.ru/home/search-results?Name=${cardName}`, (err, res, body) => {
-      if (err) { return console.log(err); }
-      response.send(body);
+  JSDOM.fromURL(`https://mtgsale.ru/home/search-results?Name=${cardName}`, {includeNodeLocations: true}).then((dom) => {
+      const pTexts = dom.window.document.documentElement.getElementsByClassName('ctclass');
+      const items = [];
+      for (let i = 0; i < pTexts.length; i++) {
+          const item = pTexts.item(i);
+
+          const priceElem = item.querySelector('.pprice'); 
+          const cardNameElem = item.querySelector('.tnamec');
+          const quantityElem = item.querySelector('.colvo');
+
+          let price = null;
+          let name = null;
+          let link = null;
+          let quantity = null;
+
+          if (priceElem) {
+            price = priceElem.textContent.trim();
+          }
+          if (cardNameElem) {
+              name = cardNameElem.textContent.trim();
+              link = 'https://mtgsale.ru/' + cardNameElem.getAttribute('href');
+          }
+          if (quantityElem) {
+              quantity = quantityElem.textContent.trim();
+          }
+          items.push({'price': price, 'name': name, 'link': link, 'quantity': quantity});
+      }
+      response.send(items);
   });
 });
 
