97533efbb83fa319cc3cb8b6f56ca266e4f6f4ce
Alexander Alyavdin <alexander.alyavdin@retailnext.net>
2020-03-03 23:02:21 +0300

    Add prettier for server
    Plus auto-correction

--- main-server/src/parsers/mtgSaleParser.ts
+++ main-server/src/parsers/mtgSaleParser.ts
@@ -8,37 +8,45 @@ const priceSelector = '.pprice';
 const cardNameSelector = '.tnamec';
 const quantitySelector = '.colvo';
 
-const getSearchUrl = (cardName : string) : string => {
-    return `${mtgSaleUrl}/home/search-results?Name=${cardName}`;
+const getSearchUrl = (cardName: string): string => {
+  return `${mtgSaleUrl}/home/search-results?Name=${cardName}`;
 };
 
-const searchCard = async (cardName : string) : Promise<Document> => {
-    return (await JSDOM.fromURL(getSearchUrl(cardName))).window.document;
+const searchCard = async (cardName: string): Promise<Document> => {
+  return (await JSDOM.fromURL(getSearchUrl(cardName))).window.document;
 };
 
-const parseSearchResult = (document : Document) : Array<ICardItem> => {
-    const searchResultElems = document.querySelectorAll(searchResultSelector);
-    const cardItems = Array.from(searchResultElems).map((item : HTMLElement) => {
-        const getPropText = (selector: string): string => {
-          const elem = item.querySelector(selector);
-          return elem && decodeURIComponent(elem.textContent.replace(/(\r\n|\n|\r)/gm, "").replace(/\s\s+/g, ' ').trim());
-        };
+const parseSearchResult = (document: Document): Array<ICardItem> => {
+  const searchResultElems = document.querySelectorAll(searchResultSelector);
+  const cardItems = Array.from(searchResultElems).map((item: HTMLElement) => {
+    const getPropText = (selector: string): string => {
+      const elem = item.querySelector(selector);
+      return (
+        elem &&
+        decodeURIComponent(
+          elem.textContent
+            .replace(/(\r\n|\n|\r)/gm, '')
+            .replace(/\s\s+/g, ' ')
+            .trim(),
+        )
+      );
+    };
 
-        const getPropAttribute = (selector : string, attr : string) : string => {
-            const elem = item.querySelector(selector);
-            return elem && elem.getAttribute(attr);
-        };
+    const getPropAttribute = (selector: string, attr: string): string => {
+      const elem = item.querySelector(selector);
+      return elem && elem.getAttribute(attr);
+    };
 
-        const linkRel = getPropAttribute(cardNameSelector, 'href');
+    const linkRel = getPropAttribute(cardNameSelector, 'href');
 
-        return {
-            name: getPropText(cardNameSelector),
-            link: linkRel ? `${mtgSaleUrl}${linkRel}` : undefined,
-            quantity: getPropText(quantitySelector),
-            price: getPropText(priceSelector)
-        };
-    });
-    return cardItems;
+    return {
+      name: getPropText(cardNameSelector),
+      link: linkRel ? `${mtgSaleUrl}${linkRel}` : undefined,
+      quantity: getPropText(quantitySelector),
+      price: getPropText(priceSelector),
+    };
+  });
+  return cardItems;
 };
 
 export default { searchCard, parseSearchResult };
