061aea5504d5eb52f38a3bc2b008b5934aa5f1c4
Alexander Alyavdin <alexander.alyavdin@retailnext.net>
2020-03-22 15:38:40 +0300

    Change background of Header

--- main-client/src/components/HeaderMain.tsx
+++ main-client/src/components/HeaderMain.tsx
@@ -1,21 +1,21 @@
 import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import { palette, PaletteProps } from '@material-ui/system';
+import { AppBar, Toolbar } from '@material-ui/core';
 import SearchInput from './SearchInput';
 
 interface HeaderProps {
   onSearch?(value: string): void;
 }
 
-const ApplicationBar = styled(AppBar)``;
+const ApplicationBar = styled(AppBar)<PaletteProps>`
+  ${palette}
+`;
 
 const HeaderMain: FunctionComponent<HeaderProps> = ({ onSearch }) => {
   return (
-    <ApplicationBar position='static'>
+    <ApplicationBar bgcolor='primary.dark' position='static'>
       <Toolbar>
-        <Typography variant='h6'>Добро пожаловать! Введите имя карты для поиска:</Typography>
         <SearchInput onSearch={(value: string): void => onSearch && onSearch(value)} />
       </Toolbar>
     </ApplicationBar>
