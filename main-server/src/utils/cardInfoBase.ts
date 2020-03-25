be8a1f9ca29afb1f998a2115945c814b900cbc6b
sevoster <sevoster@gmail.com>
2020-03-25 09:48:43 +0300

    Support descriptions for multiface cards

--- main-server/src/utils/cardInfoBase.ts
+++ main-server/src/utils/cardInfoBase.ts
@@ -1,4 +1,4 @@
-import { Cards, Card } from 'scryfall-sdk';
+import { Cards, Card, CardFace } from 'scryfall-sdk';
 
 import ICardInfo from '@shared/interfaces/ICardInfo';
 import Logger, { LogLevel } from './logger';
@@ -15,10 +15,19 @@ const autoCompleteName = async (partName: string): Promise<Array<string>> => {
 const getCardInfo = async (cardName: string): Promise<ICardInfo> => {
   return await Cards.byName(cardName, true)
     .then((card: Card) => {
+      let descriptions = [];
+      if (card.card_faces) {
+        card.card_faces.forEach((face: CardFace) => {
+          descriptions.push(face.printed_name ? `${face.printed_name} (${face.name})` : face.name);
+          descriptions.push(face.printed_text ? face.printed_text : face.oracle_text);
+        });
+      } else {
+        descriptions.push(card.printed_text ? card.printed_text : card.oracle_text);
+      }
       return {
         name: card.printed_name ? `${card.printed_name} (${card.name})` : card.name,
         imageUrl: card.image_uris && card.image_uris.large,
-        description: card.printed_text ? card.printed_text : card.oracle_text,
+        description: descriptions.join('\n'),
       };
     })
     .catch(error => {
@@ -27,4 +36,13 @@ const getCardInfo = async (cardName: string): Promise<ICardInfo> => {
     });
 };
 
-export default { autoCompleteName, getCardInfo };
+const getConvenientName = async (cardName: string): Promise<string> => {
+  return await Cards.byName(cardName, true)
+    .then((card: Card) => card.name)
+    .catch(error => {
+      logger.log(`Failed to get card name: ${error}`, LogLevel.Error);
+      return undefined;
+    });
+};
+
+export default { autoCompleteName, getCardInfo, getConvenientName };
