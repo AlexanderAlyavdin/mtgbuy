ced99ca3a60fe7fceefa0e5bf6424a254f5828de
sevoster <sevoster@gmail.com>
2020-03-25 09:52:00 +0300

    Preprocess and compare card names

--- main-server/src/searchAggregator.ts
+++ main-server/src/searchAggregator.ts
@@ -1,6 +1,7 @@
 import ICardItem from '@shared/interfaces/ICardItem';
 
 import Logger, { LogLevel } from './utils/logger';
+import CardInfoBase from './utils/cardInfoBase';
 
 import MtgSale from './parsers/mtgSaleParser';
 import MtgTrade from './parsers/mtgTradeParser';
@@ -8,7 +9,31 @@ import CardPlace from './parsers/cardPlaceParser';
 
 const logger = new Logger('SearchAggregator');
 
+const preprocessCardName = async (cardName: string): Promise<string> => {
+  cardName = await CardInfoBase.getConvenientName(cardName);
+  // Fox multiface cards
+  if (cardName) {
+    cardName = cardName.replace('//', '');
+  }
+  return cardName;
+};
+
+// Compare by words
+const compareCardNames = (leftName: string, rightName: string): boolean => {
+  const wordRegex = /[A-Za-z0-9]+/g;
+  const leftWords = leftName.match(wordRegex);
+  const rightWords = rightName.match(wordRegex);
+  return leftWords.length === rightWords.length && leftWords.every((word, index) => word === rightWords[index]);
+};
+
 const search = async (cardName: string, filterOnStock: boolean = true): Promise<Array<ICardItem>> => {
+  cardName = await preprocessCardName(cardName);
+  if (!cardName) {
+    logger.log(`No card found by name: ${cardName}`, LogLevel.Warning);
+    return [];
+  }
+  logger.log(`Card name after preprocessing: ${cardName}`);
+
   const mtgSaleSearchPromise = MtgSale.searchCard(cardName).then(result => MtgSale.parseSearchResult(result));
   const mtgTradeSearchPromise = MtgTrade.searchCard(cardName).then(result => MtgTrade.parseSearchResult(result));
   const cardPlaceSearchPromise = CardPlace.searchCard(cardName).then(result => CardPlace.parseSearchResult(result));
@@ -30,6 +55,9 @@ const search = async (cardName: string, filterOnStock: boolean = true): Promise<
   });
 
   let cardItems = cardPlaceSearchResult.concat(mtgSaleSearchResult.concat(mtgTradeSearchResult));
+
+  cardItems = cardItems.filter((item: ICardItem) => compareCardNames(item.name, cardName));
+
   if (filterOnStock) {
     cardItems = cardItems.filter((item: ICardItem) => item.quantity > 0);
   }
