c26f16e9e82d3ca347325175b0ea45efa310ee2b
sevoster <sevoster@gmail.com>
2020-03-26 00:02:55 +0300

    Support autcomplete for russian language

--- main-server/src/utils/cardInfoBase.ts
+++ main-server/src/utils/cardInfoBase.ts
@@ -6,6 +6,31 @@ import Logger, { LogLevel } from './logger';
 const logger = new Logger('CardInfo');
 
 const autoCompleteName = async (partName: string): Promise<Array<string>> => {
+  if (partName.length < 3) return [];
+
+  // Check if contains russian symbols
+  if (/[а-яА-ЯЁё]/.test(partName)) {
+    return new Promise<Array<string>>((resolve, reject) => {
+      let cards = [];
+      const searchEmitter = Cards.search(partName, { include_multilingual: true });
+      searchEmitter.on('data', (data: Card) => {
+        if (!data.printed_name) return;
+        cards.push(data.printed_name);
+        if (cards.length === 20) {
+          searchEmitter.cancel();
+        }
+      });
+      searchEmitter
+        .on('cancel', () => resolve(cards))
+        .on('end', () => resolve(cards))
+        .on('done', () => resolve(cards))
+        .on('error', err => {
+          logger.log(`Error while autocompleting cyrillic name: ${err}`, LogLevel.Error);
+          reject(err);
+        });
+    });
+  }
+
   return await Cards.autoCompleteName(partName).catch(error => {
     logger.log(`Failed to autocomplete name: ${error}`, LogLevel.Error);
     return [];
