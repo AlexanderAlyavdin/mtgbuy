b9d7aded29390de4f82a1b6e80c1cc5285d1a98b
Alexander Alyavdin <alexander.alyavdin@retailnext.net>
2020-08-20 14:32:46 +0300

    Make life easier with less strict typescript rules

--- main-client/.eslintrc.js
+++ main-client/.eslintrc.js
@@ -14,6 +14,12 @@ module.exports = {
   },
   rules: {
     'react/prop-types': [0],
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: true,
+      },
+    ],
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
